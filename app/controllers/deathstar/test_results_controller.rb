ef7e3d3f3923383549ff10abe3cdd8da00a42a0f
Zack Hobson <zack@zackhobson.com>
2013-10-29 08:44:48 -0700

    test_results: Only show 100 results per page.

--- app/controllers/deathstar/test_results_controller.rb
+++ app/controllers/deathstar/test_results_controller.rb
@@ -2,7 +2,7 @@ module Deathstar
   class TestResultsController < ApplicationController
     def index
       @test_session = test_session
-      @test_results = @test_session.test_results.order('created_at DESC').paginate(page: params[:page], per_page: 1000)
+      @test_results = @test_session.test_results.order('created_at DESC').paginate(page: params[:page], per_page: 100)
     end
 
     private
