ec612aef1f787d7e3fe3b46dfbace3aaf3f13c07
Chuck Fitzpatrick <chuck@cloudcitydevelopment.com>
2014-02-10 17:14:02 -0500

    rename method call

--- app/controllers/deathstare/end_points_controller.rb
+++ app/controllers/deathstare/end_points_controller.rb
@@ -5,7 +5,7 @@ module Deathstare
     end
 
     def reset
-      end_point.clear_cached_devices
+      end_point.clear_upstream_sessions
       flash[:notice] = "Cleared cache of devices and sessions for #{end_point.base_url}."
       redirect_to end_points_path
     end
